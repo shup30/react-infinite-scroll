a41811014e7745d8d579845241ac24950836f799
Ryzen <ryzen@ryzen-vivobook.iballbatonwifi.com>
2020-05-06 23:07:31 +0530

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `Gatsby infinite Scroll`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `@shup30`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
